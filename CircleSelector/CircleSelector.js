c5ceb904345486a04889085dbb260580fb216258
Masanori Takano <mtkn.misty@gmail.com>
2011-12-13 02:21:55 +0900

    ドラッグで回すまで． 【残】 ・コメント書く ・自動テスト書いてリファクタリング ・フリックで慣性を持っているようにくるくる回るようにする ・外側のdivのサイズを合わせる

--- CircleSelector/CircleSelector.js
+++ CircleSelector/CircleSelector.js
@@ -6,6 +6,10 @@ var CircleSelector = function(rad, tht, len){
 
 	var circleSelector = this;
 
+	/**
+	 * 本オブジェクトのあらはす円盤状において角度に応じたx, y, スケールを返す
+	 * @param angle
+	 */
 	this.getCoor = function(angle) {
 		var sina = Math.sin(angle);
 
@@ -20,14 +24,34 @@ var CircleSelector = function(rad, tht, len){
 		};
 	};
 
-	this.changeCoor = function(angle, dx) {
-		var da = -Math.atan2(dx, Math.sqrt(r * r - dx * dx));
-		console.log((da * 180 / Math.PI))
-		return circleSelector.getCoor(angle + da);
+	/**
+	 *
+	 * @param angle
+	 * @param dx
+	 */
+	this.getDeltaAngle = function(stPtx, finPtx) {
+		if(stPtx > r) {
+			stPtx = r;
+		} else if (stPtx < -r) {
+			stPtx = -r;
+		}
+
+		if(finPtx > r) {
+			finPtx = r;
+		} else if (finPtx < -r) {
+			finPtx = -r;
+		}
+		var y1 = Math.sqrt(r * r - stPtx * stPtx);
+		var y2 = Math.sqrt(r * r - finPtx * finPtx);
+		var phi1 = -Math.atan2(stPtx, y1);
+		var phi2 = Math.atan2(finPtx, y2);
+		return phi1 + phi2;
 	};
+
 };
 
 (function($){
+
 	var parent;
 	var children;
 
@@ -38,8 +62,11 @@ var CircleSelector = function(rad, tht, len){
 
 	var test = 0;
 
-	var setPos = function(dom, angle){
-		var coor = calcFunc.getCoor(angle);
+	var setPos = function(dom, angle, da){
+		if(!da) {
+			da = 0;
+		}
+		var coor = calcFunc.getCoor(angle + da);
 		var w = size * coor.scale;
 		var w2 = w / 2;
 		dom.css('width', w + 'px');
@@ -47,49 +74,45 @@ var CircleSelector = function(rad, tht, len){
 		dom.css('top', (coor.y - w / 2 + 200) + 'px');
 		dom.css('left', (coor.x - w / 2 + 200) + 'px');
 		dom.css('z-index', parseInt(coor.scale * 1000) + '');
-		dom.data('angle', angle);
+		dom.data('angle', {angle: angle, dangle: da});
 	};
 
-	var changePos = function(dom, dx) {
-		console.log(test + ', ' + dx);
-		var angle = dom.data('angle');
-		var coor = calcFunc.changeCoor(angle, dx);
-		var w = size * coor.scale;
-		var w2 = w / 2;
-		dom.css('width', w + 'px');
-		dom.css('height', w + 'px');
-		dom.css('top', (coor.y - w / 2 + 200) + 'px');
-		dom.css('left', (coor.x - w / 2 + 200) + 'px');
-		dom.css('z-index', parseInt(coor.scale * 1000) + '');
-		dom.data('angle', angle);
+	var changePos = function(stPtx, finPtx) {
+
+		//全体を回転させる角度を計算
+		var da = calcFunc.getDeltaAngle(stPtx, finPtx);
+
+		children.each(function(i, val){
+			var dom = $(val);
+			setPos(dom, dom.data('angle').angle, da);
+		});
+
 	};
 
 	var getX = function(e) {
-		return e.pageX - size / 2;
+		return e.pageX - size / 2 - 200;
 	};
 
 	var startEvent = function(e) {
 		touchFlg = true;
-		//beforeX = $(this).css('left');//getX(e);
+		originX = getX(e);
 	};
 
 	var moveEvent = function(e) {
 		if(touchFlg) {
 			var x = getX(e);
-			//$(this).css('left', x + 'px');
-			var dx = originX - x;
-
-			children.each(function(i, val){
-				var dom = $(val);
-				changePos(dom, dx);
-			});
-
-			beforeX = x;
+			changePos(originX, x);
 		}
 	};
 
 	var finEvent = function(e) {
 		touchFlg = false;
+
+		children.each(function(i, val){
+			var dom = $(val);
+			var before = dom.data('angle');
+			dom.data('angle', {angle: before.angle + before.dangle, dangle: 0});
+		});
 	};
 
 	var setEvent = function(parent, dom){
